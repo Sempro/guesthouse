0490f9862c771e8e3fed696a3f43b3ca98e4a17e
ecrmnn <danieleckermann@gmail.com>
2017-06-27 09:22:50 +0200

    test

--- test/tests.js
+++ test/tests.js
@@ -19,7 +19,7 @@ describe('Guesthouse Test Suite', () => {
   });
 
   it('should create .editorconfig', () => {
-    expect(files.indexOf('.editorconfig') === -1).to.eql(false);
+    expect(files.indexOf('.editorconfig') === -1).to.eql(true);
   });
 
   it('should create .eslintrc.js', () => {
